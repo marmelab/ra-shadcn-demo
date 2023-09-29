b4637e310db5d252300bddaaf2218e9dd11ed9ef
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2023-09-29 21:51:14 +0200

    Update testimony

--- src/layout/LoginPage.tsx
+++ src/layout/LoginPage.tsx
@@ -28,11 +28,11 @@ export const LoginPage = () => {
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
               <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
+                &ldquo;React-admin has allowed us to quickly create and evolve a
+                powerful tool that otherwise would have taken months of time and
+                effort to develop.&rdquo;
               </p>
-              <footer className="text-sm">Sofia Davis</footer>
+              <footer className="text-sm">John Doe</footer>
             </blockquote>
           </div>
         </div>
